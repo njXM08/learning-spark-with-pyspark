4817595a232587b8adcf195474313952ea897718
Xiaoming Chen <chen_xming@hotmail.com>
2021-05-19 20:47:08 +0200

    binary classification: added confusion-matrix plotting

--- src/binaryClassification.py
+++ src/binaryClassification.py
@@ -4,6 +4,7 @@ from operator import add
 import numpy as np
 import pandas as pd
 import sklearn.metrics
+from sklearn.metrics import confusion_matrix
 
 from pyspark.sql import SparkSession
 
@@ -14,6 +15,40 @@ from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.feature import VectorIndexer, IndexToString, StringIndexer, OneHotEncoder, VectorAssembler
 from pyspark.sql.functions import col
+
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+  """
+  This function prints and plots the confusion matrix.
+  Normalization can be applied by setting `normalize=True`.
+  """
+  if normalize:
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    print("Normalized confusion matrix")
+  else:
+    print('Confusion matrix, without normalization')
+    print(cm)
+  plt.imshow(cm, interpolation='nearest', cmap=cmap)
+  plt.title(title)
+  plt.colorbar()
+
+  tick_marks = np.arange(len(classes))
+  plt.xticks(tick_marks, classes, rotation=45)
+  plt.yticks(tick_marks, classes)
+
+  fmt = '.2f' if normalize else 'd'
+  thresh = cm.max() / 2.
+
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black")
+  
+  plt.tight_layout()
+  plt.ylabel('True label')
+  plt.xlabel('Predicted label')
 
 # -------- convert data to dense vectors 
 from pyspark.sql import Row
@@ -185,6 +220,26 @@ if __name__ == "__main__":
       .select('threshold').head()['threshold']
     # lr.setThreshold(bestThreshold)
     '''
+
+    class_temp = predictions.select("label").groupBy("label")\
+    .count().sort('count', ascending=False).toPandas()
+
+    class_temp = class_temp["label"].values.tolist()
+    class_names = map(str, class_temp)
+    print(class_names, class_temp)
+
+    y_true = predictions.select("label")
+    y_true.show(5)
+    y_true = y_true.toPandas()
+
+    y_pred = predictions.select("predictedLabel")
+    y_pred.show(5)
+    y_pred = y_pred.toPandas()
+    cnf_matrix = confusion_matrix(y_true, y_pred,labels=class_temp)
+
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_temp, normalize=True, title='Confusion matrix, without normalization')
+    plt.show()
 
     spark.stop()
 
